7fa8e6f7fa08ae4803168389e403f4746cd9d7c9
Vidhi Jain <vidhijain507@gmail.com>
2025-03-06 10:27:42 +0530

    [Vidhi_Jain]Add. To display Hello from BridgeLabz with a user inputted name.

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'HelloWorldApp';
   imgUrl: string ='assets/BL_logo_square_jpg.jpg';
-  url = "https://www.bridgelabz.com/"
+  url = "https://www.bridgelabz.com/";
+  userName: string = "";
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz."
