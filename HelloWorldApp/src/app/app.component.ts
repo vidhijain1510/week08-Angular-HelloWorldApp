64902b3e88be6e8840452bcf358ec2051df6eb2b
Vidhi Jain <vidhijain507@gmail.com>
2025-03-06 11:55:33 +0530

    [Vidhi_Jain]Add. To display Hello from BridgeLabz with a user inputted name

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -13,6 +13,7 @@ export class AppComponent {
   imgUrl: string ='assets/BL_logo_square_jpg.jpg';
   url = "https://www.bridgelabz.com/";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz."
@@ -21,5 +22,17 @@ export class AppComponent {
   onClick($event : MouseEvent){
     console.log("Save Button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event: InputEvent) {
+    console.log("change Event Occurred!", ($event.target as HTMLInputElement).value);
+    const nameRegex =RegExp('^[A-Z][a-zA-Z\s]{2,}$'); // Starts with uppercase, min 3 chars
+  
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+      this.nameError = "Name is Incorrect!";
+    
   }
 }
